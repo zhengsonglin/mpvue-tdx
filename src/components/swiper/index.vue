479f48498194a87f31515715fd38cd2d69cee076
zsl <zheng.sl@belle.com.cn>
2020-05-29 18:50:11 +0800

    首页

---
+++ src/components/swiper/index.vue
@@ -0,0 +1,54 @@
+<template>
+	<swiper :indicator-dots="indicatorDots" indicator-color="rgba(255,255,255, .3)" indicator-active-color="#ffffff" :autoplay="true" :interval="autoplayTime" :current="index" :circular="true"
+		:style="{'height':height}">
+	      <block v-for="(item, index) in datas" :key="index" >
+		        <swiper-item>
+		          	<image :src="item" mode="widthFix"></image>
+		        </swiper-item>
+		    </block>
+    </swiper>
+</template>
+
+<script>
+	export default{
+		name:"MySwiper",
+		props:{
+			autoplayTime: {
+				type: String|Number,
+				default: 3000,
+			},
+			height:{
+				type: String|Number,
+				default:"160px"
+			},
+			datas:{
+				type: Array,
+				default: ()=>{
+					return [
+						require('@/assets/img/s-1.jpg'),
+						require('@/assets/img/s-2.jpg'),
+						require('@/assets/img/s-3.jpg'),
+						require('@/assets/img/s-4.jpg'),
+					]
+				}
+			}
+		},
+		data(){
+			return {
+				index:0,
+				indicatorDots:true,
+				imgUrls: [
+			        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/newsPicture/05558951-de60-49fb-b674-dd906c8897a6',
+			        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/coursePicture/0fbcfdf7-0040-4692-8f84-78bb21f3395d',
+			        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/management-school-picture/7683b32e-4e44-4b2f-9c03-c21f34320870'
+			    ]
+			}
+		},
+		methods:{
+			
+		}
+	}
+</script>
+
+<style>
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,32 +1,29 @@
 <template>
 	<div class="home">
-		home
-		<van-tabbar :active="active" @change="onChange">
-		  <van-tabbar-item icon="home-o">标签1</van-tabbar-item>
-		  <van-tabbar-item icon="search">标签2</van-tabbar-item>
-		  <van-tabbar-item icon="friends-o">标签3</van-tabbar-item>
-		  <van-tabbar-item icon="setting-o">标签4</van-tabbar-item>
-		</van-tabbar>
+		<my-swiper></my-swiper>
+		 
 	</div>
 </template>
 
 <script>
+	import mySwiper from '@/components/swiper'
 	export default{
 		name: "home",
+		components: {
+		    mySwiper
+		},
 		data(){
 			return {
-				active: 0
+				active: 0,
+				indicatorDots: true,
+				autoplay: true,
+				interval: 3000,
+				duration: 200,
+				background: ['demo-text-1', 'demo-text-2', 'demo-text-3']
 			}
 		},
 		methods:{
-			onChange(event) {
-				console.log(event)
-				this.active = event.mp.detail
-			    wx.showToast({
-			      title: `点击标签 ${event.mp.detail + 1}`,
-			      icon: 'none',
-			    });
-			},
+			
 		}
 	}
 </script>
